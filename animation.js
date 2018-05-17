4487727a801f3ab5fce0390cc9c63c7a469592bb
Claudia  Munoz <cm16321@srcs.loc>
2018-05-17 12:15:47 -0700

    js file

---
+++ animation.js
@@ -0,0 +1,32 @@
+var vibratingbox = document.getElementById("jsanimation");
+var xpos1 = 200;
+var ypos1 = 400;
+var xstep1 = 46;
+var ystep1 = 60;
+var id = setInterval(move, 10);
+
+function move() {
+    if (xpos1 > 350 || xpos1 > 0) {
+        xstep1 = -xstep1;
+    }
+    if (ypos1 < 300 || ypos1 > 0) {
+        ystep1 = -ystep1;
+        setInterval(vibratingbox, 1000);
+    }
+    xpos1 = xpos1 + xstep1;
+    ypos1 = ypos1 + ystep1;
+    vibratingbox.style.top = ypos1 + 'px';
+    vibratingbox.style.left = xpos1 + 'px';
+}
+
+window.addEventListener("keypress", change);
+
+function change(event) {
+    if (event.key === "a"){
+        vibratingbox.message = "Boo"
+    } else if (event.key === "s") {
+        vibratingbox.style.background = "orange";
+    } else {
+        vibratingbox.style.background = "green";
+    }
+}
